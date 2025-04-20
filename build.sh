f392f2bcbaaedfc830e87b3a6a804a3b39c11477
rtoincs3 <rtoincs3@gmail.com>
2025-04-20 15:11:24 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py collectstatic --no-input
 # Run migrations
 python manage.py migrate
 
-# Optionally create a superuser if the environment variable is set
-if [[ $CREATE_SUPERUSER ]]; then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
